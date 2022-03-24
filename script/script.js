d1383b0a9bf101a4625ff8d4d710ea2db74e2147
Irina Pyanova <kizaru001@gmail.com>
2022-03-24 17:46:36 +0300

    Удаление функции показа даты

--- script/script.js
+++ script/script.js
@@ -14,35 +14,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
   scrollToTop();
 
-  const leadBtn = document.querySelector('.btn-lead');
-  const dateInfo = document.querySelector('.date-info');
-
-  function getDayInfo(date) {
-    const monthNames = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
-    const daysNames = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-
-    const dateArr = date.split('.');
-    const now = new Date (dateArr[1] + '.' + dateArr[0] + '.' + dateArr[2]);
-    const year = now.getFullYear();
-    const month = monthNames[now.getMonth()];
-    const dateNow = now.getDate();
-    const day = now.getDay();
-    const dayName = daysNames[now.getDay()];
-
-    const weekOfMonth = Math.ceil((dateNow + 6 - day)/7);
-
-    let newDate = dayName + ', ' + weekOfMonth + ' ' + 'неделя ' + month + ' ' + year + ' года';
-    dateInfo.textContent = newDate;
-    console.log(newDate);
-  }
-
-  leadBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    getDayInfo('08.03.2022');
-    dateInfo.style.display = 'block';
-  });
-
-  getDayInfo('08.03.2022');
-  getDayInfo('28.02.2022');
-
 });
